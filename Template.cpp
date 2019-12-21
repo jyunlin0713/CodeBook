d626c4949d916b1d3af62251ea135f8b50e551b8
TigerWolf0713 <36495051+TigerWolf0713@users.noreply.github.com>
2019-12-21 13:45:09 +0800

    Update Template.cpp

--- Template.cpp
+++ Template.cpp
@@ -12,20 +12,16 @@ using namespace std;
 #define s second
 #define MOD 1000000007
 #define mkp make_pair
-#define M_PI 3.14159265358979323846
 #define FOR(i,l,r) for (int i=l;i<=r;i++)
 #define LOR(i,l,r) for (ll i=l;i<=r;i++)
 #define FORD(i,r,l) for (int i=r;i>=l;i--)
 #define LORD(i,r,l) for (ll i=r;i>=l;i--)
-#define INF 1000000000
 #define CL(x) memset(x,0,sizeof(x))
 #define DBG(x) cout << #x << " : " << x << '\n'
 #define ALL(x) x.begin(), x.end()
 #define SZ(x) x.size()
 #define UI(x) (int)(x-'A')
 #define LI(x) (int)(x-'a')
-#define lc(x) x*2
-#define rc(x) x*2+1
 #define LL_MAX (1LL<<60)
 #define DB 0
 typedef long long ll;
@@ -69,20 +65,16 @@ using namespace std;
 #define s second
 #define MOD 1000000007
 #define mkp make_pair
-#define M_PI 3.14159265358979323846
 #define FOR(i,l,r) for (int i=l;i<=r;i++)
 #define LOR(i,l,r) for (ll i=l;i<=r;i++)
 #define FORD(i,r,l) for (int i=r;i>=l;i--)
 #define LORD(i,r,l) for (ll i=r;i>=l;i--)
-#define INF 1000000000
 #define CL(x) memset(x,0,sizeof(x))
 #define DBG(x) cout << #x << " : " << x << '\n'
 #define ALL(x) x.begin(), x.end()
 #define SZ(x) x.size()
 #define UI(x) (int)(x-'A')
 #define LI(x) (int)(x-'a')
-#define lc(x) x*2
-#define rc(x) x*2+1
 #define LL_MAX (1LL<<60)
 #define DB 0
 typedef long long ll;
