f0b798304996ed13a91b0c050aa97e316d182a77
TigerWolf0713 <36495051+TigerWolf0713@users.noreply.github.com>
2020-01-16 14:21:56 +0800

    Update Template.cpp

--- Template.cpp
+++ Template.cpp
@@ -152,3 +152,99 @@ pll modinv(ll x, ll y)
 auto seed = chrono::high_resolution_clock::now().time_since_epoch().count();
 std::mt19937_64 mt(seed);
 //usage : mt() returns a random number
+
+//range sum, single add
+struct
+{
+    void up(int id)
+    {
+        st[id] = st[id*2] + st[id*2+1];
+    }
+    void build(int l, int r, int id)
+    {
+        if(l==r)
+        {
+            st[id]=0;
+            return;
+        }
+        int m = (l+r)/2;
+        build(l,m,id*2);
+        build(m+1,r,id*2+1);
+        up(id);
+    }
+    ll Sum(int L, int R, int l, int r, int id)
+    {
+        if( l>=L && r<=R )return st[id];
+        if( l>R || r<L )return 0;
+        int m = (l+r)/2;
+        return Sum(L,R,l,m,id*2)+Sum(L,R,m+1,r,id*2+1);
+    }
+    void Add(int p, ll x, int l, int r, int id)
+    {
+        if(l==r)
+        {
+            st[id]+=x;
+            return;
+        }
+        int m = (l+r)/2;
+        if(p<=m)Add(p,x,l,m,id*2);
+        else Add(p,x,m+1,r,id*2+1);
+        up(id);
+    }
+    ll st[MXN*4];
+}segt1;
+
+//range sum, range add
+struct
+{
+    ll Get(int id){ return st[id]+ad[id]; }
+    void up(int id)
+    {
+        st[id] = Get(id*2) + Get(id*2+1);
+    }
+    void down(int l, int r, int id)
+    {
+        ll val = ad[id];
+        ad[id] = 0;
+        st[id]+=val;
+        if(l==r)return;
+        ad[id*2]+=val, ad[id*2+1]+=val;
+    }
+    void build(int l, int r, int id)
+    {
+        ad[id] = 0;
+        if(l==r)
+        {
+            st[id]=0;
+            return;
+        }
+        int m = (l+r)/2;
+        build(l,m,id*2);
+        build(m+1,r,id*2+1);
+        up(id);
+    }
+    ll Sum(int L, int R, int l, int r, int id)
+    {
+        down(l,r,id);
+        if( l>=L && r<=R )return st[id];
+        if( l>R || r<L )return 0;
+        int m = (l+r)/2;
+        return Sum(L,R,l,m,id*2)+Sum(L,R,m+1,r,id*2+1);
+    }
+    void Add(int L, int R, ll x, int l, int r, int id)
+    {
+        down(l,r,id);
+        if( l>=L && r<=R )
+        {
+            ad[id]+=x;
+            return;
+        }
+        if( l>R || r<L )return;
+
+        int m = (l+r)/2;
+        Add(L,R,x,l,m,id*2);
+        Add(L,R,x,m+1,r,id*2+1);
+        up(id);
+    }
+    ll st[MXN*4], ad[MXN*4];
+}segt2;
