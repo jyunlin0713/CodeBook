54adea9a871fc45f988ac71cf42b034f2cab88aa
TigerWolf0713 <36495051+TigerWolf0713@users.noreply.github.com>
2020-12-10 19:13:40 +0800

    Update Template.cpp

--- Template.cpp
+++ Template.cpp
@@ -41,6 +41,14 @@ int main()
     return 0;
 }
 
+/* stuff you should look for
+	* int overflow, array bounds
+	* special cases (n=1?)
+	* do smth instead of nothing and stay organized
+	* WRITE STUFF DOWN
+	* DON'T GET STUCK ON ONE APPROACH
+*/
+
 //==============================================
 //MACRO 2
 #include <bits/stdc++.h>
