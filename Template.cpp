967c54433dfafcbc07edef4e1be9f0f01f97de57
TigerWolf0713 <36495051+TigerWolf0713@users.noreply.github.com>
2021-04-18 18:33:33 +0800

    Update Template.cpp

--- Template.cpp
+++ Template.cpp
@@ -244,3 +244,24 @@ struct
     }
     ll st[MXN*4], ad[MXN*4];
 }segt2;
+
+struct DSU {
+	int pa[MAXN], sz[MAXN];
+	void init(int n) {
+		for (int i = 0; i <= n; i++) {
+			pa[i] = i;
+			sz[i] = 1;
+		}
+	}
+	int find(int x) {
+		return x == pa[x] ? x : find(pa[x]); // Add compression by yourself
+	}
+	int merge(int x, int y) {
+		x = find(x); y = find(y);
+		if (x == y) return 0;
+		if (sz[x] < sz[y]) swap(x, y);
+		sz[x] += sz[y];
+		pa[y] = x;
+		return 1;
+	}
+} dsu;
