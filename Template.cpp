3a2592fa3500b2a23375421619766636bd297781
TigerWolf0713 <36495051+TigerWolf0713@users.noreply.github.com>
2022-01-19 15:08:12 +0800

    Update Template.cpp

--- Template.cpp
+++ Template.cpp
@@ -2,31 +2,18 @@
 using namespace std;
 #define pb push_back
 #define pii pair<int,int>
-#define pll pair<ll, ll>
-#define pil pair<int,ll>
-#define pli pair<ll,int>
-#define ppi pair<pii,int>
-#define pip pair<int,pii>
-#define ppp pair<pii,pii>
-#define pdd pair<double, double>
-#define f first
-#define s second
+#define fi first
+#define sc second
 #define MOD 1000000007
 #define mkp make_pair
 #define FOR(i,l,r) for (int i=l;i<=r;i++)
-#define LOR(i,l,r) for (ll i=l;i<=r;i++)
 #define FORD(i,r,l) for (int i=r;i>=l;i--)
-#define LORD(i,r,l) for (ll i=r;i>=l;i--)
 #define CL(x) memset(x,0,sizeof(x))
 #define ALL(x) x.begin(), x.end()
 #define SZ(x) (int)(x.size())
-#define UI(x) (int)(x-'A')
-#define LI(x) (int)(x-'a')
-#define DI(x) (int)(x-'0')
 #define LL_MAX (1LL<<61)
 #define FND(S,x) (S.find(x)!=S.end())
 #define bit(x,y) (!!(((ll)x)&(1LL<<y)))
-#define inv(x) modexp(x,MOD-2)
 typedef long long ll;
 typedef long double ld;
 
